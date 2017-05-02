33daf9a49cff771275ed344f03090eb15fead1bb
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-01 21:47:54 -0400

    Addig step 7

--- ajax.js
+++ ajax.js
@@ -9,18 +9,27 @@ $(document).ready(function() {
   });
   $('#step3456 button').on('click', function(){
     $.ajax({
-      url: 'http://first-ajax-api.herokuapp.com/pong',
+      url: 'http://first-ajax-api.herokuapp.com/ping',
       method: 'GET',
       dataType: 'text'
     }).done(function(responseData){
       console.log(responseData);
       $('#step3456').append('<p>' + responseData + '</p>');
     }).fail(function(){
-      console.log("failed");
+      console.log('Request failed');
       $('#step3456').append('<p> Sorry, we try harder next time!</p>');
     }).always(function(){
       console.log('Hey the request finished!');
     });
   });
 
+  $('#step7 button').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      dataType: 'text'
+    }).done(function(responseData){
+      $('#step7').append('<p>' + responseData + '</p>');
+    });
+  });
 });
